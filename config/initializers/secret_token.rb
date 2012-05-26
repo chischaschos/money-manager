5d7ddcdcc306cbd6554c740be40921c41f4aa8d7
emmanuel delgado <emmanuel.delgado@crowdint.com>
2012-05-25 21:25:05 -0500

    * First setup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MoneyManager::Application.config.secret_token = 'f7c5a3d25f50f277d07848d3eeb6493d2bef5f4981da57a8fae8dadffbb5145418ff40197ac09996538b129d1d976ee80600d891ce4acd76c9b40b34c08dbb47'
